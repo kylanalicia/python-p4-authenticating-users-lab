13534ebab271af9b40e4586b33328b1adc276a25
aliciakimani.ky@gmail.com <aliciakimani.ky@gmail.com>
2023-09-29 13:47:57 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -1,57 +1,52 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify, request, session
-from flask_migrate import Migrate
+from flask import Flask, request, jsonify, session
 from flask_restful import Api, Resource
 
-from models import db, Article, User
-
 app = Flask(__name__)
-app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
-migrate = Migrate(app, db)
-
-db.init_app(app)
-
+app.secret_key = 'your_secret_key'  # Set a secret key for session management
 api = Api(app)
 
-class ClearSession(Resource):
+# Sample user data (you can replace this with your own user data)
+users = [{'id': 1, 'username': 'user1'}, {'id': 2, 'username': 'user2'}]
 
+# Resource for user login
+class LoginResource(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        
+        # Find the user by username (assuming usernames are unique)
+        user = next((user for user in users if user['username'] == username), None)
+
+        if user:
+            session['user_id'] = user['id']
+            return jsonify(user), 200
+        else:
+            return '', 401
+
+# Resource for user logout
+class LogoutResource(Resource):
     def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
+        if 'user_id' in session:
+            session.pop('user_id')
+        return '', 204
 
-        return {}, 204
-
-class IndexArticle(Resource):
-    
+# Resource for checking the session
+class CheckSessionResource(Resource):
     def get(self):
-        articles = [article.to_dict() for article in Article.query.all()]
-        return articles, 200
+        user_id = session.get('user_id')
+        
+        if user_id is not None:
+            user = next((user for user in users if user['id'] == user_id), None)
+            if user:
+                return jsonify(user), 200
+        
+        return '', 401
 
-class ShowArticle(Resource):
-
-    def get(self, id):
-        session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
-        session['page_views'] += 1
-
-        if session['page_views'] <= 3:
-
-            article = Article.query.filter(Article.id == id).first()
-            article_json = jsonify(article.to_dict())
-
-            return make_response(article_json, 200)
-
-        return {'message': 'Maximum pageview limit reached'}, 401
-
-api.add_resource(ClearSession, '/clear')
-api.add_resource(IndexArticle, '/articles')
-api.add_resource(ShowArticle, '/articles/<int:id>')
-
+api.add_resource(LoginResource, '/login')
+api.add_resource(LogoutResource, '/logout')
+api.add_resource(CheckSessionResource, '/check_session')
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True)
